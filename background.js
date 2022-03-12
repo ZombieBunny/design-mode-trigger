d5c6a9859e45885455240a85d8a5635c4962c5ad
Lee <leevashan.reddi@aax.co.za>
2022-03-12 07:52:15 +0200

    updated button state, updated icon

--- background.js
+++ background.js
@@ -1,10 +1,24 @@
 
 let mode = false;
+let disabled = {
+    16: "/images/dmi_disable_16.png",
+    32: "/images/dmi_disable_32.png"
+};
+let enabled = {
+    16: "/images/dmi_enabled_16.png",
+    32: "/images/dmi_enabled_32.png"
+};
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ mode });
 });
+
 chrome.action.onClicked.addListener((tab) => {
     mode = !mode;
     chrome.storage.sync.set({ mode });
-    chrome.tabs.sendMessage(tab.id, { mode });
+    chrome.tabs.sendMessage(tab.id, { mode }, updateIcon(tab, mode));
 });
+
+function updateIcon(tab, mode) {
+    chrome.action.setIcon({ path: mode ? enabled : disabled, tabId: tab.id });
+}
